5b8b860882dc5f477ac84a1f1b2e4e2d627eb4fd
amine-aboufirass <amine.aboufirass@gmail.com>
2022-12-05 09:53:48 +0100

    adding setup.py with some dependencies and some cli functionality

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="pandoc-latex-writer",
+    name="pandoc_latex_writer",
     description="practical markdown to latex conversion using pandoc",
     author="Amine Aboufirass",
     author_email="amine.aboufirass@gmail.com",
@@ -9,6 +9,12 @@ setup(
         "panflute>=2.2.3",
         "beautifulsoup4>=4.11.1",
         "requests>=2.28.1",
-        "urllib3>=1.26.12"
-    ]
+        "urllib3>=1.26.12",
+        "Pygments"
+    ],
+    entry_points={
+        "console_scripts": [
+            "pandoc-latex-writer=pandoc_latex_writer:cli.main"
+        ]
+    }
 )
